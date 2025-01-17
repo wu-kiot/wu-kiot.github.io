08fb336b388391f743c66a3cd4b7d14a59422f41
seyyaw <seidymam@gmail.com>
2025-01-17 22:16:44 +0000

    Deploying to gh-pages from @ wu-kiot/wu-kiot.github.io@0c1014ebd5a64820209f8d8c8f07424144afd40b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -513,7 +513,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://wu-kiot.github.io/", "_blank");
         },
       },{
       id: 'light-theme',
