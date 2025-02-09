4916e078080dbaa7c59f5007923b28e8aea4d2b0
seyyaw <seidymam@gmail.com>
2025-02-09 01:48:40 +0000

    Deploying to gh-pages from @ wu-kiot/wu-kiot.github.io@c91f8fed6798eb1b15028d9ebb6f3edfb3089dc6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "Welcome to the WU/KIoT GitHub repository details page. Here, you will see lists of the members and public repositories.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
